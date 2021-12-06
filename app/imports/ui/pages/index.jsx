1e4ecd88d2cd23728e73637224ca27a8673b62c5
Vincent Fontesse <vincent.fontesse@ac-dijon.fr>
2021-12-06 15:28:04 +0100

    feat(index): new files for app cards

--- app/imports/ui/pages/index.jsx
+++ app/imports/ui/pages/index.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
-import { Roles } from 'meteor/alanning:roles';
-import { useAppContext } from '../contexts/context';
+// import { Roles } from 'meteor/alanning:roles';
+import { makeStyles } from '@material-ui/core/styles';
+
+// import { useAppContext } from '../contexts/context';
+import AppCard from '../components/AppCard';
+
+const useStyles = makeStyles(() => ({
+  cardContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+}));
 
 export default function Index() {
-  const [{ user }] = useAppContext();
-  const isAdmin = user ? Roles.userIsInRole(user._id, 'admin') : false;
-
-  return <div>{`${isAdmin ? 'Bonjour maître' : 'Coucou'} ${user ? user.firstName : ''}`}</div>;
+  // const [{ user }] = useAppContext();
+  // const isAdmin = user ? Roles.userIsInRole(user._id, 'admin') : false;
+  const classes = useStyles();
+  return (
+    <div>
+      <main>
+        <span className={classes.cardContainer}>
+          <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
+          <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
+          <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
+          <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
+          <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
+          <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
+          <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
+          <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
+          <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
+          <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
+          <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
+          <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
+          <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
+          <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
+          <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
+          <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
+          <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
+          <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
+        </span>
+      </main>
+    </div>
+  );
 }
