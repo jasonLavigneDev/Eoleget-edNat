6fdd7164de94f513bf7d6696fd9970a3c2b87475
Vincent Fontesse <vincent.fontesse@ac-dijon.fr>
2021-12-06 15:49:42 +0100

    feat(index): first layout for app cards

--- app/imports/ui/pages/index.jsx
+++ app/imports/ui/pages/index.jsx
@@ -4,8 +4,17 @@ import { makeStyles } from '@material-ui/core/styles';
 
 // import { useAppContext } from '../contexts/context';
 import AppCard from '../components/AppCard';
+import TopBar from '../components/menus/TopBar';
 
 const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    position: 'relative',
+  },
+  main: {
+    display: 'flex',
+    marginTop: '50px',
+  },
   cardContainer: {
     display: 'flex',
     flexDirection: 'row',
@@ -19,8 +28,9 @@ export default function Index() {
   // const isAdmin = user ? Roles.userIsInRole(user._id, 'admin') : false;
   const classes = useStyles();
   return (
-    <div>
-      <main>
+    <div className={classes.root}>
+      <TopBar />
+      <main className={classes.main}>
         <span className={classes.cardContainer}>
           <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
           <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
