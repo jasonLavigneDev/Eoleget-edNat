ae4a2e75cf8c35af92361a7eeaac567404b0f902
Vincent Fontesse <vincent.fontesse@ac-dijon.fr>
2021-12-07 15:09:30 +0100

    feat(store): add a search bar for app store

---
+++ app/imports/ui/components/search/SearchBarApp.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import i18n from 'meteor/universe:i18n';
+import PropTypes from 'prop-types';
+
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyles = makeStyles(() => ({
+  searchBar: {
+    width: '100%',
+  },
+}));
+
+function SearchBarApp({ opened }) {
+  const [open, setOpen] = useState(false);
+  const classes = useStyles();
+
+  React.useEffect(() => {
+    setOpen(opened);
+  }, [opened]);
+
+  return (
+    <TextField
+      label={i18n.__('components.Search.searchingApp')}
+      variant="outlined"
+      className={classes.searchBar}
+      open={open}
+      onOpen={() => {
+        setOpen(true);
+      }}
+      onClose={() => {
+        setOpen(false);
+      }}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchIcon />
+          </InputAdornment>
+        ),
+      }}
+    />
+  );
+}
+
+SearchBarApp.defaultProps = {
+  opened: false,
+};
+
+SearchBarApp.propTypes = {
+  opened: PropTypes.bool,
+};
+
+export default SearchBarApp;
--- app/imports/ui/pages/index.jsx
+++ app/imports/ui/pages/index.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import i18n from 'meteor/universe:i18n';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import Collapse from '@material-ui/core/Collapse';
+import SearchIcon from '@material-ui/icons/Search';
+import IconButton from '@material-ui/core/IconButton';
 
 import AppCard from '../components/card/AppCard';
 import TopBar from '../components/menus/TopBar';
+import SearchBarApp from '../components/search/SearchBarApp';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -17,6 +21,14 @@ const useStyles = makeStyles(() => ({
     flexDirection: 'column',
     marginTop: '50px',
   },
+  storeTitleContainer: {
+    width: 'fit-content',
+  },
+  storeTitleContent: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
   cardContainer: {
     display: 'flex',
     flexWrap: 'wrap',
@@ -24,14 +36,26 @@ const useStyles = makeStyles(() => ({
 }));
 
 export default function Index() {
+  const [showSearchApp, setShowSearchApp] = useState(false);
   const classes = useStyles();
+
   return (
     <div className={classes.root}>
       <TopBar />
       <main className={classes.main}>
-        <Typography variant="h4" component="div">
-          {i18n.__('pages.Store.storeTitle')}
-        </Typography>
+        <div className={classes.storeTitleContainer}>
+          <div className={classes.storeTitleContent}>
+            <Typography variant="h4" component="div">
+              {i18n.__('pages.Store.storeTitle')}
+            </Typography>
+            <IconButton onClick={() => setShowSearchApp(!showSearchApp)}>
+              <SearchIcon fontSize="large" />
+            </IconButton>
+          </div>
+          <Collapse in={showSearchApp} collapsedSize={0}>
+            <SearchBarApp opened={showSearchApp} />
+          </Collapse>
+        </div>
         <span className={classes.cardContainer}>
           <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
           <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
