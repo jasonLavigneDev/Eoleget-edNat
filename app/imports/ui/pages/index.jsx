788c61ebe5f95ff7a2035b1a01aa8b276e332cc2
Vincent Fontesse <vincent.fontesse@ac-dijon.fr>
2021-12-07 15:09:30 +0100

    feat(store): add a search bar for app store

--- app/imports/ui/pages/index.jsx
+++ app/imports/ui/pages/index.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import i18n from 'meteor/universe:i18n';
 // import { Roles } from 'meteor/alanning:roles';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-// import { useAppContext } from '../contexts/context';
+import Collapse from '@material-ui/core/Collapse';
+import SearchIcon from '@material-ui/icons/Search';
+import IconButton from '@material-ui/core/IconButton';
 
+// import { useAppContext } from '../contexts/context';
 import AppCard from '../components/card/AppCard';
 import TopBar from '../components/menus/TopBar';
+import SearchBarApp from '../components/search/SearchBarApp';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -19,6 +23,14 @@ const useStyles = makeStyles(() => ({
     flexDirection: 'column',
     marginTop: '50px',
   },
+  storeTitleContainer: {
+    width: 'fit-content',
+  },
+  storeTitleContent: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
   cardContainer: {
     display: 'flex',
     flexWrap: 'wrap',
@@ -28,14 +40,26 @@ const useStyles = makeStyles(() => ({
 export default function Index() {
   // const [{ user }] = useAppContext();
   // const isAdmin = user ? Roles.userIsInRole(user._id, 'admin') : false;
+  const [showSearchApp, setShowSearchApp] = useState(false);
   const classes = useStyles();
+
   return (
     <div className={classes.root}>
       <TopBar />
       <main className={classes.main}>
-        <Typography variant="h4" component="div">
-          {i18n.__('pages.Store.storeTitle')}
-        </Typography>
+        <div className={classes.storeTitleContainer}>
+          <div className={classes.storeTitleContent}>
+            <Typography variant="h4" component="div">
+              {i18n.__('pages.Store.storeTitle')}
+            </Typography>
+            <IconButton onClick={() => setShowSearchApp(!showSearchApp)}>
+              <SearchIcon fontSize="large" />
+            </IconButton>
+          </div>
+          <Collapse in={showSearchApp} collapsedSize={0}>
+            <SearchBarApp opened={showSearchApp} />
+          </Collapse>
+        </div>
         <span className={classes.cardContainer}>
           <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
           <AppCard title="La classe" subTitle="C'est vrai" content="C'est pas mal" />
