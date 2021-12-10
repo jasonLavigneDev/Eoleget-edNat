ce91ba32c49c431bb5555fff02bb9e890be201b9
Vincent Fontesse <vincent.fontesse@ac-dijon.fr>
2021-12-10 14:10:55 +0100

    feat(packs): add a display mode list for packs

--- app/imports/ui/components/packsCard/packCard.jsx
+++ app/imports/ui/components/packsCard/packCard.jsx
@@ -1,58 +1,107 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, styled } from '@material-ui/core/styles';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import IconButton from '@material-ui/core/IconButton';
+import OpenInNewIcon from '@material-ui/icons/OpenInNew';
+import Tooltip from '@material-ui/core/Tooltip';
 
 import AppPacksCard from './appPacksCard';
 import lightTheme from '../../themes/light';
 
 const useStyles = makeStyles((theme) => ({
+  cardContainer: {
+    margin: '1%',
+  },
   card: {
     position: 'relative',
     width: '28em',
-    height: '42em',
+    height: 'auto',
     margin: '1%',
     backgroundColor: lightTheme.palette.primary.light,
     boxShadow: theme.shadows[3],
   },
   cardHeader: {
+    display: 'flex',
     background: 'linear-gradient(#6e1bdc,#f138a7 100%)',
     color: 'white',
   },
-  cardContent: {
-    display: 'flex',
-    flexDirection: 'column',
+  expendMore: {
+    marginRight: '42%',
   },
-  cardActions: {
-    position: 'absolute',
-    bottom: '0',
-    right: '32%',
+  iconButton: {
+    color: 'white',
   },
 }));
 
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
 function PackCard() {
+  const [showMore, setShowMore] = useState(false);
   const classes = useStyles();
 
+  React.useEffect(() => {
+    setShowMore(showMore);
+  }, [showMore]);
+
   return (
-    <Card className={classes.card}>
-      <CardHeader title="Pack" className={classes.cardHeader} />
-      <CardContent className={classes.cardContent}>
-        <AppPacksCard />
-        <AppPacksCard />
-        <AppPacksCard />
-        <AppPacksCard />
-        <AppPacksCard />
-        <CardActions className={classes.cardActions}>
-          <Button variant="contained" size="large">
-            Voir plus
-          </Button>
+    <div className={classes.cardContainer}>
+      <Card className={classes.card}>
+        <CardHeader
+          title="Pack"
+          className={classes.cardHeader}
+          action={
+            <Tooltip title="Voir le pack">
+              <IconButton className={classes.iconButton}>
+                <OpenInNewIcon />
+              </IconButton>
+            </Tooltip>
+          }
+        />
+        <CardContent>
+          <AppPacksCard />
+          <AppPacksCard />
+          <Collapse in={showMore} unmountOnExit>
+            <AppPacksCard />
+            <AppPacksCard />
+            <AppPacksCard />
+            <AppPacksCard />
+            <AppPacksCard />
+            <AppPacksCard />
+            <AppPacksCard />
+          </Collapse>
+        </CardContent>
+        <CardActions>
+          <Tooltip title="Etendre le pack">
+            <ExpandMore
+              expand={showMore}
+              onClick={() => {
+                setShowMore(!showMore);
+              }}
+              aria-expanded={showMore}
+              aria-label="show more"
+              className={classes.expendMore}
+            >
+              <ExpandMoreIcon fontSize="large" />
+            </ExpandMore>
+          </Tooltip>
         </CardActions>
-      </CardContent>
-    </Card>
+      </Card>
+    </div>
   );
 }
 
