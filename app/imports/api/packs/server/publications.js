df31f68d6c07683148276f79bc306f0b8301dd18
Vincent Fontesse <vincent.fontesse@ac-dijon.fr>
2021-12-13 15:50:19 +0100

    feat(publish): add function for publication

---
+++ app/imports/api/packs/server/publications.js
@@ -0,0 +1,16 @@
+import { Meteor } from 'meteor/meteor';
+// import { isActive } from '../../utils';
+import Packs from '../packs';
+
+// A supprimer quand le fichier utils.js sera commit
+function isActive() {
+  return false;
+}
+
+// publish all packs
+Meteor.publish('packs.all', function packsAll() {
+  if (!isActive(this.userId)) {
+    return this.ready();
+  }
+  return Packs.find({}, { fields: Packs.publicFields, sort: { name: 1 }, limit: 5000 });
+});
