6d4fa706db79048fc29c20b886c7a3b2d3fc3787
Vincent Fontesse <vincent.fontesse@ac-dijon.fr>
2022-01-24 11:53:14 +0100

    feat(style): add dropdown list for versions in detail app

---
+++ app/imports/ui/components/version/listVersion.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Select, FormControl, MenuItem } from '@mui/material';
+
+// Styles CSS //
+const formControlStyle = {
+  width: 150,
+};
+// End style //
+
+function ListVersion({ versions }) {
+  const [version, setVersion] = React.useState('');
+
+  const handleChange = (event) => {
+    setVersion(event.target.value);
+  };
+  return (
+    <FormControl sx={formControlStyle}>
+      <Select value={version} inputProps={{ 'aria-label': 'Versions' }} displayEmpty onChange={handleChange}>
+        <MenuItem value="">latest</MenuItem>
+        {versions
+          .sort()
+          .reverse()
+          .map((vers) => (
+            <MenuItem value={vers}>{vers}</MenuItem>
+          ))}
+      </Select>
+    </FormControl>
+  );
+}
+
+ListVersion.propTypes = {
+  // eslint-disable-next-line react/no-unused-prop-types
+  versions: PropTypes.arrayOf.isRequired,
+};
+
+export default ListVersion;
