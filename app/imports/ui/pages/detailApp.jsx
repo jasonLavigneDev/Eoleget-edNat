224dfbe281e16ce4ef2b5320ec31fec2da79facf
Vincent Fontesse <vincent.fontesse@ac-dijon.fr>
2022-05-09 16:35:15 +0200

    refactor(ui): fix version number position in detail app

--- app/imports/ui/pages/detailApp.jsx
+++ app/imports/ui/pages/detailApp.jsx
@@ -164,7 +164,7 @@ const detailApp = ({ app, ready }) => {
               <div style={{ display: 'flex', marginTop: 10 }}>
                 <Typography style={{ paddingRight: 5, paddingTop: 5, paddingBottom: 10 }}>Versions :</Typography>
                 {checkAppAllreadyAdded() ? (
-                  <p>{getVersion()}</p>
+                  <div style={{ paddingTop: 5 }}>{getVersion()}</div>
                 ) : (
                   <ListVersion versions={app.versions} app={app} setCommand={setCommand} />
                 )}
