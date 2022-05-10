0504c52bfdc456f219aaff3dd02f2e15cea0c423
Vincent Fontesse <vincent.fontesse@ac-dijon.fr>
2022-05-10 10:29:47 +0200

    refactor(cart): desable hover when button is not active

--- app/imports/ui/components/appCart/appCart.jsx
+++ app/imports/ui/components/appCart/appCart.jsx
@@ -60,7 +60,7 @@ function AppCart({ cart }) {
       <Paper
         sx={{
           '&:hover': {
-            backgroundColor: 'secondary.main',
+            backgroundColor: isDisable ? 'none' : 'secondary.main',
           },
         }}
       >
